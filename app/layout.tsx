6117e6f21a38b40a87c8558cd59033c186503caa
clarencetan19 <woshiclalen@gmail.com>
2025-10-23 02:17:31 +0800

    feat: initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
-import NavigationBar from "@/components/navigation-bar";
 import Footer from "@/components/footer";
+import NavigationBar from "@/components/navigation-bar";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
